a0767e6b29d3d42023ca3d6aed1b8b89c4875df9
MickDarkstar <mikael.fehrm@live.se>
2019-06-26 23:24:52 +0200

    testat entry/get med token som header: authorization

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../shared/services/auth.service';
+import { Subject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-content',
@@ -6,10 +10,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-content.component.scss']
 })
 export class MainContentComponent implements OnInit {
+  private url = 'http://localhost/gym-app-server/api/controllers/entry/'
 
-  constructor() { }
+  private dataSource = new Subject<any>()
+
+  constructor(private authService: AuthService, private http: HttpClient) { }
 
   ngOnInit() {
+    this.authService.currentUser.subscribe(() => {
+      if (this.authService.isLoggedIn()) {
+
+        const httpOptions = {
+          headers: new HttpHeaders({
+            'Content-Type': 'application/json',
+            Authorization: localStorage.getItem('id_token')
+          }),
+          withCredentials: false
+        }
+
+        this.http.get<any>(this.url + 'get.php', httpOptions)
+          .pipe(map(result => {
+            this.dataSource = result.data
+          }))
+          .subscribe()
+      }
+    })
   }
 
 }
