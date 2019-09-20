f23ab808b5233d72dd6c28c2843c96c4fde53fe7
MickDarkstar <mikael.fehrm@live.se>
2019-09-20 15:15:05 +0200

    removed console log

--- src/app/_helpers/api.interceptor.ts
+++ src/app/_helpers/api.interceptor.ts
@@ -6,11 +6,7 @@ import { environment } from 'src/environments/environment';
 @Injectable()
 export class ApiInterceptor implements HttpInterceptor {
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        const apiUrl = environment.apiUrl
-        console.log('req url:' + req.url);
-        console.log('api url:' + apiUrl);
-
-        const apiRequest = req.clone({ url: `${apiUrl}${req.url}` })
+        const apiRequest = req.clone({ url: `${environment.apiUrl}${req.url}` })
         return next.handle(apiRequest)
     }
 }
